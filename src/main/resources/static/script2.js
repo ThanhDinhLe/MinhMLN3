55e7904b8b1a4dc36321bdc37bed2be7bd1361d4
ThanhDinhLe <thanhldhe186993@fpt.edu.vn>
2026-09-15 17:22:17 +0700

    Update puzzle answers

--- src/main/resources/static/script2.js
+++ src/main/resources/static/script2.js
@@ -71,7 +71,7 @@ checkBtn.addEventListener("click", () => {
 
     const answer = answerInput.value.trim().toLowerCase();
 
-    if(answer === "gia ca tang"){
+    if(answer === "dan lam chu"){
 
         window.location.href = "index3.html";
 
--- src/main/resources/static/script4.js
+++ src/main/resources/static/script4.js
@@ -71,7 +71,7 @@ checkBtn.addEventListener("click", () => {
 
     const answer = answerInput.value.trim().toLowerCase();
 
-    if(answer === "ngan phieu"){
+    if(answer === "nhan quyen"){
 
         window.location.href = "index5.html";
 
