b74305b41016cbe3c2885fc36f708a6229d84257
ThanhDinhLe <thanhldhe186993@fpt.edu.vn>
2026-09-15 23:44:22 +0700

    Revert "Update puzzle answers"
    This reverts commit 55e7904b8b1a4dc36321bdc37bed2be7bd1361d4.

--- src/main/resources/static/script4.js
+++ src/main/resources/static/script4.js
@@ -71,7 +71,7 @@ checkBtn.addEventListener("click", () => {
 
     const answer = answerInput.value.trim().toLowerCase();
 
-    if(answer === "nhan quyen"){
+    if(answer === "ngan phieu"){
 
         window.location.href = "index5.html";
 
